bf26ec13828a9b14b2688f1bddfa27801df8ff9e
Rafael Albuquerque <rafael.albuquerquev@gmail.com>
2025-03-26 09:52:57 -0300

    Implementação inicial do Diamante de X (v 1.0)

--- DiamanteDeX.ConsoleApp/Program.cs
+++ DiamanteDeX.ConsoleApp/Program.cs
@@ -1,8 +1,10 @@
-﻿namespace DiamanteDeX.ConsoleApp
+﻿using System.ComponentModel.Design;
+
+namespace DiamanteDeX.ConsoleApp
 {
     internal class Program
     {
-        static void Main(string[] args)           
+        static void Main(string[] args)
 
         {
             while (true)
@@ -18,7 +20,7 @@
                 string x8 = "xxxxxxxx";
                 string x9 = "xxxxxxxxx";
                 string x10 = "xxxxxxxxx";
-                string x11 = "xxxxxxxxxx";
+                string x11 = "xxxxxxxxxxx";
 
 
 
@@ -28,29 +30,108 @@
                 Console.WriteLine();
 
                 Console.WriteLine("Por favor, digite um número ímpar entre 3 e 11: ");
-                int userNumber = 0;
+                int userNumber = Convert.ToInt32(Console.ReadLine());
 
 
                 if (userNumber % 2 == 0)
                 {
-                    Console.WriteLine("Número inválido. Por favor, insira apenas números pares.");
+                    Console.WriteLine("Número inválido. Por favor, insira apenas números ímpares.");
                 }
 
                 else {
                     if (userNumber == 3)
                     {
+                        Console.WriteLine("x---------------------------x--------------------------------x----------------------------x");
                         Console.WriteLine();
+                        Console.WriteLine(" " + x1);
+                        Console.WriteLine(x3);
+                        Console.WriteLine(" " + x1);
+                        Console.WriteLine();
+                        Console.WriteLine("x---------------------------x--------------------------------x----------------------------x");
                     }
 
+                    else if (userNumber == 5)
+                    {
+                        Console.WriteLine("x---------------------------x--------------------------------x----------------------------x");
+                        Console.WriteLine();
+                        Console.WriteLine("  " + x1);
+                        Console.WriteLine(" " + x3);
+                        Console.WriteLine(x5);
+                        Console.WriteLine(" " + x3);
+                        Console.WriteLine("  " + x1);
+                        Console.WriteLine();
+                        Console.WriteLine("x---------------------------x--------------------------------x----------------------------x");
+                    }
+
+                    else if (userNumber == 7)
+                    {
+                        Console.WriteLine("x---------------------------x--------------------------------x----------------------------x");
+                        Console.WriteLine();
+                        Console.WriteLine("   " + x1);
+                        Console.WriteLine("  " + x3);
+                        Console.WriteLine(" " + x5);
+                        Console.WriteLine(x7);
+                        Console.WriteLine(" " + x5);
+                        Console.WriteLine("  " + x3);
+                        Console.WriteLine("   " + x1);
+                        Console.WriteLine();
+                        Console.WriteLine("x---------------------------x--------------------------------x----------------------------x");
+                    }
+
+                    else if (userNumber == 9)
+                    {
+
+                        Console.WriteLine("x---------------------------x--------------------------------x----------------------------x");
+                        Console.WriteLine();
+                        Console.WriteLine("    " + x1);
+                        Console.WriteLine("   " + x3);
+                        Console.WriteLine("  " + x5);
+                        Console.WriteLine(" " + x7);
+                        Console.WriteLine(x9);
+                        Console.WriteLine(" " + x7);
+                        Console.WriteLine("  " + x5);
+                        Console.WriteLine("   " + x3);
+                        Console.WriteLine("    " + x1);
+                        Console.WriteLine();
+                        Console.WriteLine("x---------------------------x--------------------------------x----------------------------x");
+                    }
+
+
+                    else if (userNumber == 11)
+                    {
+                        Console.WriteLine("x---------------------------x--------------------------------x----------------------------x");
+                        Console.WriteLine();
+                        Console.WriteLine("     " + x1);
+                        Console.WriteLine("    " + x3);
+                        Console.WriteLine("   " + x5);
+                        Console.WriteLine("  " + x7);
+                        Console.WriteLine(" " + x9);
+                        Console.WriteLine(x11);
+                        Console.WriteLine(" " + x9);
+                        Console.WriteLine("  " + x7);
+                        Console.WriteLine("   " + x5);
+                        Console.WriteLine("    " + x3);
+                        Console.WriteLine("     " + x1);
+                        Console.WriteLine();
+                        Console.WriteLine("x---------------------------x--------------------------------x----------------------------x");
+                    }
+
+                    else if (userNumber < 1 && userNumber > 11)
+                    {
+
+                      Console.WriteLine("Número inválido. Digite apenas números ímpares entre 1 e 11.");
+
+                    }                 
+                        
+                
                 }
-
-
-                    Console.ReadLine();
-
-
+                Console.ReadLine();
             }
 
         }
-              
+
     }
 }
+              
+    
+
