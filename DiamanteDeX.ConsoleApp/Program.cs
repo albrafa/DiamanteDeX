18180794a43196675de1bdc3370d3e2a4ccd3455
Rafael Albuquerque <rafael.albuquerquev@gmail.com>
2025-03-26 09:15:47 -0300

    Add project files.

---
+++ DiamanteDeX.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DiamanteDeX.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("x---------------------------x--------------------------------x----------------------------x");
+            Console.WriteLine("                                     DIAMANTE DE X");
+            Console.WriteLine("x---------------------------x--------------------------------x----------------------------x");
+
+
+            Console.ReadLine();
+        }
+    }
+}
