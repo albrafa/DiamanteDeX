26d7cfa6356a1137cfdb187a3cf9911945b507b3
Rafael Albuquerque <rafael.albuquerquev@gmail.com>
2025-03-26 09:32:00 -0300

    Início do projeto Diamante de X

--- DiamanteDeX.ConsoleApp/Program.cs
+++ DiamanteDeX.ConsoleApp/Program.cs
@@ -2,14 +2,55 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)           
+
         {
-            Console.WriteLine("x---------------------------x--------------------------------x----------------------------x");
-            Console.WriteLine("                                     DIAMANTE DE X");
-            Console.WriteLine("x---------------------------x--------------------------------x----------------------------x");
+            while (true)
+            {
+
+                string x1 = "x";
+                string x2 = "xx";
+                string x3 = "xxx";
+                string x4 = "xxxx";
+                string x5 = "xxxxx";
+                string x6 = "xxxxxx";
+                string x7 = "xxxxxxx";
+                string x8 = "xxxxxxxx";
+                string x9 = "xxxxxxxxx";
+                string x10 = "xxxxxxxxx";
+                string x11 = "xxxxxxxxxx";
 
 
-            Console.ReadLine();
+
+                Console.WriteLine("x---------------------------x--------------------------------x----------------------------x");
+                Console.WriteLine("                                     DIAMANTE DE X");
+                Console.WriteLine("x---------------------------x--------------------------------x----------------------------x");
+                Console.WriteLine();
+
+                Console.WriteLine("Por favor, digite um número ímpar entre 3 e 11: ");
+                int userNumber = 0;
+
+
+                if (userNumber % 2 == 0)
+                {
+                    Console.WriteLine("Número inválido. Por favor, insira apenas números pares.");
+                }
+
+                else {
+                    if (userNumber == 3)
+                    {
+                        Console.WriteLine();
+                    }
+
+                }
+
+
+                    Console.ReadLine();
+
+
+            }
+
         }
+              
     }
 }
